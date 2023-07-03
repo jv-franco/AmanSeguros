12fd17d3f26b8d2ef5a0fa209077ea6ab4d9131c
Jvfrango <jv_franco@outlook.com>
2023-07-03 17:57:16 -0300

    btn error

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,9 @@
 export function Button({ props }) {
   return (
-    <a className="sm:w-auto w-full flex items-center justify-center" href="/">
+    <a
+      className="sm:w-auto w-full flex items-center justify-center"
+      href="#main"
+    >
       <button data-aos="fade-left" className="btn uppercase ">
         {props}
       </button>
